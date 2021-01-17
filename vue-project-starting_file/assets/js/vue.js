18c7faa6ae7e5a467eaaadd6c757a9a0070901f2
LaylaBouteiller <layla.bouteiller@gmail.com>
2021-01-17 20:44:36 +0100

    more components + vuerouter

--- vue-project-starting_file/assets/js/vue.js
+++ vue-project-starting_file/assets/js/vue.js
@@ -4,6 +4,24 @@ const Home = {
   name: 'Home'
 }
 
+const UserSettings = {
+  template: '<h1>UserSettings</h1>',
+  name: 'UserSettings'
+}
+
+const WishList = {
+  template: '<h1>WishList</h1>',
+  name: 'WishList'
+}
+
+const ShoppingCart = {
+  template: '<h1>ShoppingCart</h1>',
+  name: 'ShoppingCart'
+}
+
+//Router
+const router = new VueRouter;
+
 //Instance de vue
 const vue = new Vue({
   router
